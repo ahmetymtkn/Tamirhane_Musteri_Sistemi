5fde24de1cfa07478d0531ced38dd0319227509c
Ahmet Yumutkan <113467735+ahmetymtkn@users.noreply.github.com>
2025-06-14 13:32:05 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -10,7 +10,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'deneme789');
+define('DB_NAME', 'tamirhane');
 
 // PDO bağlantısının oluşturulması
 try {
